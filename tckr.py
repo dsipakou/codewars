d3937ac12d3b94673994a2c7f1a70ba3cbeffedd
Dzianis Sipakou <mc.flyer@gmail.com>
2020-01-07 15:27:21 +0300

    Start

---
+++ tckr.py
@@ -0,0 +1,14 @@
+def ticker(text, width, tick):
+    return ''
+
+print(ticker("Hello", 6, 5))
+
+# "Hello " -> 6
+# "ello  " -> 7
+# "llo   " -> 8
+# "lo    " -> 9
+# "o     " -> 10
+# "      " -> 11 -> 0
+# "     H" -> 12 -> 1
+
+#
