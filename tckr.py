03aff6b416c3b7450f7ddd55fdb3aa5568b238ea
Dzianis Sipakou <mc.flyer@gmail.com>
2020-01-07 16:03:55 +0300

    End

--- tckr.py
+++ tckr.py
@@ -1,14 +1,28 @@
 def ticker(text, width, tick):
-    return ''
+    end_char = tick % (width + len(text))
+    part_text = text[max(0, end_char - width):end_char]
+    return " " * (width - end_char) + part_text + " " * (end_char - len(text))
 
-print(ticker("Hello", 6, 5))
+print(ticker("Hello there", 6, 16))
 
+# "      " -> 0
+# "     H" -> 1
+# "    He" -> 2
+# "   Hel" -> 3
+# "  Hell" -> 4
+# " Hello" -> 5
 # "Hello " -> 6
-# "ello  " -> 7
-# "llo   " -> 8
-# "lo    " -> 9
-# "o     " -> 10
-# "      " -> 11 -> 0
-# "     H" -> 12 -> 1
+# "ello t" -> 7
+# "llo th" -> 8
+# "lo the" -> 9
+# "o ther" -> 10
+# " there" -> 11
+# "there " -> 12
+# "here  " -> 13
+# "ere   " -> 14
+# "re    " -> 15
+# "e     " -> 16
+# "      " -> 17 -> 0
+# "     H" -> 18 -> 1
 
 #
