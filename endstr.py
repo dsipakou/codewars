3efdf0797a54b0d084771bdbe2fa97ba9ce4db30
Dzianis Sipakou <mc.flyer@gmail.com>
2020-01-10 15:42:27 +0300

    Continue

--- endstr.py
+++ endstr.py
@@ -1,8 +1,12 @@
 def endless_string(string, start, length):
     calc_start = start % len(string)
-    return string[start:]
+    remain = length - (length - calc_start)
+    whole = remain // length
+    end = remain % length
+    print(remain, whole, end)
+    return string[calc_start:]
 
-print(endless_string('asdf', -3, 4))
+print(endless_string('asdf', 3, 4))
 
 
 
