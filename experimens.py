9644db25fa1e7a94c347ea90ea617cf364256f80
Dzianis Sipakou <mc.flyer@gmail.com>
2020-01-13 11:27:55 +0300

    New

--- experimens.py
+++ experimens.py
@@ -4,19 +4,27 @@ def quickSort(array):
 def main_job(array, start, end):
     print(start, end)
     if start > end:
-    	return
+        return
     pivot = start
     left = start + 1
     right = end
     while left <= right:
-    	if array[left] >= array[pivot] and array[right] < array[pivot]:
-    		array[left], array[right] = array[right], array[left]
-    	if array[left] < array[pivot]:
-    		left += 1
-    	if array[right] >= array[pivot]:
-    		right -=1
+        if array[left] >= array[pivot] and array[right] < array[pivot]:
+                array[left], array[right] = array[right], array[left]
+        if array[left] < array[pivot]:
+                left += 1
+        if array[right] >= array[pivot]:
+                right -=1
     array[pivot], array[right] = array[right], array[pivot]
     main_job(array, start, right - 1)
     main_job(array, right + 1, end)
     return array
-print(quickSort([6,5,7,5,4,23,2]))
+
+def caesar(string, key):
+    output = ""
+    for i in string:
+        current_chr = (ord(i) - 97 + key) % 26
+        output += chr(current_chr + 97)
+    return output
+
+print(caesar("xyz", 2))
