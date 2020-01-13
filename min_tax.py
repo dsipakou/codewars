8c472612206797dfca5e349f812c3aa9fc34960c
Dzianis Sipakou <mc.flyer@gmail.com>
2020-01-13 15:02:02 +0300

    faster

--- min_tax.py
+++ min_tax.py
@@ -1,10 +1,11 @@
 def min_num_taxis(requests):
-    left = min([x for x, y in requests])
-    right = max([y for x, y in requests])
-    output = [0 for x in range(left, right + 1)]
+    d = {}
     for i in requests:
         for j in range(i[0], i[1]):
-            output[j - left] += 1
-    return max(output)
+            d[j] = d.get(j, 0) + 1
+    current_max = 0
+    for v in d.values():
+        current_max = max(current_max, v)
+    return current_max
 
 print(min_num_taxis([(1,4), (2,5), (1, 10), (4,5)]))
