2c894f1e27c07b2f58656609b38b83d17d37392d
Dzianis Sipakou <mc.flyer@gmail.com>
2020-01-15 10:20:48 +0300

    Update

--- divs.py
+++ divs.py
@@ -1,6 +1,5 @@
 def divisors(n):
-    output = 0
-    for i in range(n, 0, -1):
-        if n % i == 0:
-            output += 1
-    return output
+    return sum(1 for i in range(n, 0, -1) if n % i == 0)
+
+
+print(divisors(17))
