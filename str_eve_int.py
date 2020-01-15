2ccf69c67b3416c669e6a66cd5ee6f050fb0c63f
Dzianis Sipakou <mc.flyer@gmail.com>
2020-01-15 16:49:29 +0300

    Update

--- str_eve_int.py
+++ str_eve_int.py
@@ -1,13 +1,27 @@
+from datetime import datetime
+
 def strongest_even(n, m):
+    mem = {}
     start = n
+    strongest = n
+    max_even = 0
+    start_time = datetime.now()
     if n % 2 == 1:
         start = n + 1
-    for i in range(n, m + 1):
+    for i in range(start, m + 1, 2):
         a = i
-        index = 0
-        while a % 2 == 0:
-            index += 1
-            a /= 2
-        print("Number is: ", i, " and strongest even is: ", index)
+        index = helper(mem, i)
+        if index > max_even:
+            strongest = i
+            max_even = index
+    return strongest
 
-print(strongest_even(10,28))
+def helper(mem, n):
+    if n in mem:
+        return mem[n]
+    if n % 2 == 1:
+        return 0
+    mem[n] = 1 + helper(mem, n / 2)
+    return mem[n]
+
+print(strongest_even(10,5880000))
