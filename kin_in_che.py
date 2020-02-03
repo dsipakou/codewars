510f96dd8e5036e7462ddd606acac850865c58e9
Dzianis Sipakou <mc.flyer@gmail.com>
2020-02-03 16:56:08 +0300

    Update

--- kin_in_che.py
+++ kin_in_che.py
@@ -17,23 +17,49 @@ def king(chessboard):
                 return True
         if fig_item[0] == '♜' or fig_item[0] == '♛':
             if (king[0] - fig_item[1][0]) == 0 or (king[1] - fig_item[1][1]) == 0:
-                return True
+                empty = True
+                if king[0] == fig_item[1][0]:
+                    print("Vertical")
+                    start = min(king[1], fig_item[1][1]) + 1
+                    end = max(king[1], fig_item[1][1])
+                    for i in range(start, end):
+                        print(chessboard[king[0]][i])
+                        if chessboard[king[0]][i] != ' ':
+                            empty = False
+                            break
+                else:
+                    print("Horizont")
+                    start = min(king[0], fig_item[1][0]) + 1
+                    end = max(king[0], fig_item[1][0])
+                    print(king, fig_item, start, end)
+                    for i in range(start, end):
+                        print(i, king[1])
+                        print(chessboard[i][king[1]])
+                        if chessboard[i][king[1]] != ' ':
+                            empty = False
+                            break
+                if empty:
+                    return True
         if fig_item[0] == '♟':
             if (king[0] - fig_item[1][0]) == 1 and abs(king[1] - fig_item[1][1]) == 1:
                 return True
         if fig_item[0] == '♞':
-            if (abs(king[0] - fig_item[1][0]) == 1 and abs(king[1] - fig_item[1][1] == 2)) or (abs(king[0] - fig_item[1][0]) == 2 and abs(king[1] - fig_item[1][1]) == 1):
+            print(king, fig_item)
+            left = abs(king[0] - fig_item[1][0])
+            right = abs(king[1] - fig_item[1][1])
+            print(left, right)
+            if (left == 1 and right == 2) or (left == 2 and right == 1):
                 return True
     return False
 
 
 print(king([
             [' ',' ',' ',' ',' ',' ',' ',' '],
+            [' ',' ',' ',' ',' ','♜',' ',' '],
+            [' ','♜',' ',' ',' ',' ',' ',' '],
             [' ',' ',' ',' ',' ',' ',' ',' '],
             [' ',' ',' ',' ',' ',' ',' ',' '],
             [' ',' ',' ',' ',' ',' ',' ',' '],
-            [' ',' ',' ','♟',' ',' ',' ',' '],
-            [' ',' ','♔',' ',' ',' ',' ',' '],
-            [' ',' ',' ',' ',' ',' ',' ',' '],
-            [' ',' ',' ',' ',' ',' ',' ',' ']
+            [' ',' ','♞',' ',' ',' ',' ',' '],
+            ['♔',' ',' ',' ',' ',' ',' ',' ']
         ]))
