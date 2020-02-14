8d80c980a0014f8377439ddf46c3857f6a6b41cd
Dzianis Sipakou <mc.flyer@gmail.com>
2020-02-14 09:59:34 +0300

    Update

--- mak_spir.py
+++ mak_spir.py
@@ -9,12 +9,13 @@ def spiral(size):
     while not stop:
         output[i][j] = 1
         if current_dir == 0:
+            print(i, j)
             if j < size - 2 and output[i][j + 2] != 1:
                 changed_direction = False
                 j += 1
-            elif j < size - 1:
-                changed_direction = False
-                j += 1
+ #           elif j < size - 1:
+ #               changed_direction = False
+ #               j += 1
             else:
                 if changed_direction:
                     stop = True
@@ -25,8 +26,8 @@ def spiral(size):
             if i < size - 2 and output[i + 2][j] != 1:
                 changed_direction = False
                 i += 1
-            elif i < size - 1:
-                i += 1
+#            elif i < size - 1:
+#                i += 1
             else:
                 if changed_direction:
                     stop = True
@@ -37,8 +38,8 @@ def spiral(size):
             if j > 1 and output[i][j - 2] != 1:
                 changed_direction = False
                 j -= 1
-            elif j > 0:
-                j -= 1
+ #           elif j > 0:
+ #               j -= 1
             else:
                 if changed_direction:
                     stop = True
@@ -46,17 +47,26 @@ def spiral(size):
                     current_dir = (current_dir + 1) % 4
                     changed_direction = True
         else:
+            print(i, j)
             if i > 1 and output[i - 2][j] != 1:
+                print(output[i - 2][j])
                 changed_direction = False
                 i -= 1
-            elif i > 0:
-                i -= 1
+#            elif i > 0:
+#                changed_direction = False
+#                i -= 1
             else:
+                print('else')
                 if changed_direction:
                     stop = True
                 else:
+                    print('change', i, j)
                     current_dir = (current_dir + 1) % 4
                     changed_direction = True
     return output
 
-print(spiral(4))
+def check_near(arr, i, j):
+    pass
+
+for i in spiral(10):
+    print(i)
