2294f784c2deb941a77feeebf7f94ffeb7f4649b
Dzianis Sipakou <mc.flyer@gmail.com>
2020-02-14 10:25:17 +0300

    End

--- mak_spir.py
+++ mak_spir.py
@@ -9,13 +9,12 @@ def spiral(size):
     while not stop:
         output[i][j] = 1
         if current_dir == 0:
-            print(i, j)
-            if j < size - 2 and output[i][j + 2] != 1:
+            if j < size - 2 and output[i][j + 2] != 1 and output[i + 1][j + 1] != 1:
                 changed_direction = False
                 j += 1
- #           elif j < size - 1:
- #               changed_direction = False
- #               j += 1
+            elif i == 0 and j < size - 1:
+                changed_direction = False
+                j += 1
             else:
                 if changed_direction:
                     stop = True
@@ -23,11 +22,11 @@ def spiral(size):
                     current_dir = (current_dir + 1) % 4
                     changed_direction = True
         elif current_dir == 1:
-            if i < size - 2 and output[i + 2][j] != 1:
+            if i < size - 2 and output[i + 2][j] != 1 and output[i + 1][j - 1] != 1:
                 changed_direction = False
                 i += 1
-#            elif i < size - 1:
-#                i += 1
+            elif j == size - 1 and i < size - 1:
+                i += 1
             else:
                 if changed_direction:
                     stop = True
@@ -35,11 +34,12 @@ def spiral(size):
                     current_dir = (current_dir + 1) % 4
                     changed_direction = True
         elif current_dir == 2:
-            if j > 1 and output[i][j - 2] != 1:
+            if j > 1 and output[i][j - 2] != 1 and output[i - 1][j - 1] != 1:
                 changed_direction = False
                 j -= 1
- #           elif j > 0:
- #               j -= 1
+            elif i == size - 1 and j > 0:
+                changed_direction = False
+                j -= 1
             else:
                 if changed_direction:
                     stop = True
@@ -47,20 +47,13 @@ def spiral(size):
                     current_dir = (current_dir + 1) % 4
                     changed_direction = True
         else:
-            print(i, j)
-            if i > 1 and output[i - 2][j] != 1:
-                print(output[i - 2][j])
+            if i > 1 and output[i - 2][j] != 1 and output[i - 1][j + 1] != 1:
                 changed_direction = False
                 i -= 1
-#            elif i > 0:
-#                changed_direction = False
-#                i -= 1
             else:
-                print('else')
                 if changed_direction:
                     stop = True
                 else:
-                    print('change', i, j)
                     current_dir = (current_dir + 1) % 4
                     changed_direction = True
     return output
@@ -68,5 +61,5 @@ def spiral(size):
 def check_near(arr, i, j):
     pass
 
-for i in spiral(10):
+for i in spiral(8):
     print(i)
