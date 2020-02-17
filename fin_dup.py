191d716a0e9e00080abf41e8426243b1cf4d9f50
Dzianis Sipakou <mc.flyer@gmail.com>
2020-02-17 11:29:13 +0300

    End

--- fin_dup.py
+++ fin_dup.py
@@ -2,9 +2,9 @@ def dcount(text):
     d = {}
     output = 0
     for c in text:
-        d[c] = d.get(c, 0)
-        d[c] += 1
-        if d[c] == 2:
+        d[c.lower()] = d.get(c.lower(), 0)
+        d[c.lower()] += 1
+        if d[c.lower()] == 2:
             output += 1
     return output
 
