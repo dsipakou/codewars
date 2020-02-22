45870a83fec7d89f7a5871a11d8a07355b449ddc
Dzianis Sipakou <mc.flyer@gmail.com>
2020-02-22 08:54:18 +0300

    update

--- men_boy.py
+++ men_boy.py
@@ -1,13 +1,13 @@
 def man(arr):
-    men = []
-    boys = []
+    men = set()
+    boys = set()
     for item in arr:
         if item % 2 == 0:
-            men.append(item)
+            men.add(item)
         else:
-            boys.append(item)
-    men.sort()
-    boys.sort(reverse=True)
+            boys.add(item)
+    men = sorted(men)
+    boys = sorted(boys, reverse=True)
     return men + boys
 
 print(man([82,91,72,76,76,100,85]))
