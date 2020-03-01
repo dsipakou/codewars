bbbdcfddc83cffcff20045b2f1d7262682d4266f
Dzianis Sipakou <mc.flyer@gmail.com>
2020-03-01 10:14:58 +0300

    End

--- per_rec.py
+++ per_rec.py
@@ -1,18 +1,15 @@
-import sys
-sys.setrecursionlimit(10**6)
-
 def per_rec(n):
-    d = {
-        0: 0,
-        1: 1,
-        2: 1,
-    }
-    helper(n, d)
-    return sum(d.values()) * 4
+    first = 1
+    second = 1
+    summ = first + second
+    for i in range(2, n):
+        first, second = second, first+second
+        summ += second
+    return summ * 4
 
 def helper(n, d):
     if n not in d:
         d[n] = helper(n - 1, d) + helper(n - 2, d)
     return d[n]
 
-print(per_rec(41988))
+print(per_rec(8))
