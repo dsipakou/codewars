5f2e01fbedbed36c0be51bf2725134d495336df3
Dzianis Sipakou <mc.flyer@gmail.com>
2020-03-10 10:45:16 +0300

    Continue'

--- yes_no.py
+++ yes_no.py
@@ -1,4 +1,20 @@
 def yes_no(arr):
-    output = []
+    output = [False for _ in range(len(arr))]
+    output1 = []
+    while False in output:
+        i = 0
+        take = True
+        while i < len(arr):
+            if take and not output[i]:
+                take = False
+                output[i] = True
+                output1.append(arr[i])
+            elif not output[i]:
+                take = True
+            i += 1
+    return output1
+
+
+
 
 print(yes_no([1,2,3,4,5,6,7]))
