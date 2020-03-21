d06281a5f07a2fec36bc75f8fd58a0806059e40c
Dzianis Sipakou <mc.flyer@gmail.com>
2020-03-21 12:17:54 +0300

    End

--- pl_one.py
+++ pl_one.py
@@ -1,12 +1,20 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        if digits[-1] == 9:
-            digits[-1] = 0
-            if len(digits) == 1:
-                digits.append(1)
-                return digits[::-1]
+        index = 0
+        store = 0
+        while index < len(digits):
+            current_index = -index - 1
+            if digits[current_index] == 9:
+                digits[current_index] = 0
+                store = 1
+            elif store == 1:
+                digits[current_index] += 1
+                store = 0
+                break
             else:
-                digits[-2] += 1
-        else:
-                digits[-1] += 1
+                digits[current_index] += 1
+                break
+            index += 1
+        if store == 1:
+            return [1] + digits
         return digits
