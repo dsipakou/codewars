9d9a544232f892019e754b48cb4aeecdb01fe6fd
Dzianis Sipakou <mc.flyer@gmail.com>
2020-03-24 11:26:09 +0300

    Update

--- mer_sor.py
+++ mer_sor.py
@@ -3,14 +3,20 @@ class Solution:
         l1 = 0
         l2 = 0
         while l1 < len(nums1) and l2 < len(nums2):
-            if nums1[l1] >= nums2[l2]:
+            if m == 0:
+                nums1[l1] = nums2[l2]
+                l1 += 1
+                l2 += 1
+            elif nums1[l1] >= nums2[l2]:
                 for i in range(len(nums1) - 1, l1, -1):
                     nums1[i] = nums1[i - 1]
                 nums1[l1] = nums2[l2]
                 l2 += 1
-            elif nums1[l1] == 0:
+            elif (m == 0) or (nums1[l1] == 0 and nums1[l1 - 1] > nums1[l1]):
                 nums1[l1] = nums2[l2]
                 l2 += 1
+                if m == 0:
+                    l1 += 1
             else:
                 l1 += 1
         return nums1
