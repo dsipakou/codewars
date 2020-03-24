d2a26e4fa58f893665500ec7f14087f88ab7c728
Dzianis Sipakou <mc.flyer@gmail.com>
2020-03-24 11:29:52 +0300

    End

--- mer_sor.py
+++ mer_sor.py
@@ -1,22 +1,4 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        l1 = 0
-        l2 = 0
-        while l1 < len(nums1) and l2 < len(nums2):
-            if m == 0:
-                nums1[l1] = nums2[l2]
-                l1 += 1
-                l2 += 1
-            elif nums1[l1] >= nums2[l2]:
-                for i in range(len(nums1) - 1, l1, -1):
-                    nums1[i] = nums1[i - 1]
-                nums1[l1] = nums2[l2]
-                l2 += 1
-            elif (m == 0) or (nums1[l1] == 0 and nums1[l1 - 1] > nums1[l1]):
-                nums1[l1] = nums2[l2]
-                l2 += 1
-                if m == 0:
-                    l1 += 1
-            else:
-                l1 += 1
-        return nums1
+        nums1[m:] = nums2
+        nums1.sort()
