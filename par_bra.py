ea8937696b37a1726b76fcbdbc63f7708be6a10a
Dzianis Sipakou <mc.flyer@gmail.com>
2020-04-02 10:52:17 +0300

    Cont

--- par_bra.py
+++ par_bra.py
@@ -1,20 +1,24 @@
 def balanced_parens(n):
     output = []
-    return solve(n, output)
+    answ = solve(n, output)
+    return list(filter(lambda item: item.count("(") == n, answ))
     
-def solve(n, output):
+def solve(n, output = []):
     if n == 0:
         return [""]
     if n == 1:
         return ["()"]
     if n == 2:
         return ["()()", "(())"]
-    for el in solve(n - 1, output):
+    for el in solve(n - 1):
         output.append("()" + el)
         output.append(el + "()")
         output.append("(" + el + ")")
     
     return list(set(output))
 
-
-print(balanced_parens(4))
+answer = balanced_parens(7)
+print(answer)
+print(len(answer))
+answer = list(filter(lambda item: item.count("(") == 7, answer))
+print(len(answer))
