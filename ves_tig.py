290c8e81397e3980e2aff4b3763bbce71f3b4872
Dzianis Sipakou <mc.flyer@gmail.com>
2020-04-04 10:28:35 +0300

    New

---
+++ ves_tig.py
@@ -0,0 +1,13 @@
+t = int(input())
+for tt in range(t):
+    n = int(input())
+    rows = cols = 0
+    matrix = [list(map(int, input().split())) for _ in range(n)]
+    rot_matrix = [[matrix[i][j] for i in range(n)] for j in range(n)]
+    summ = sum(matrix[i][i] for i in range(n))
+    for i in range(n):
+        if len(set(matrix[i])) < n:
+            rows += 1
+        if len(set(rot_matrix[i])) < n:
+            cols += 1
+    print(f"Case #{tt + 1}: {summ} {rows} {cols}")
