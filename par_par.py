a78691f25023f47bb27df479dea56b0bcccd9101
Dzianis Sipakou <mc.flyer@gmail.com>
2020-04-04 12:12:29 +0300

    End

--- par_par.py
+++ par_par.py
@@ -1,21 +1,21 @@
 t = int(input())
 for tt in range(t):
     n = int(input())
-    intervals = sorted(list(map(int, input().split())) for ___ in range(n))
+    intervals = sorted([list(map(int, input().split())), i] for i in range(n))
     c = 0
     j = 0
-    output = ""
+    output = []
     impossible = False
     for interval in intervals:
-        if interval[0] >= c:
-            output += "C"
-            c = interval[1]
-        elif interval[0] >= j:
-            output += "J"
-            j = interval[1]
+        if interval[0][0] >= c:
+            output.append([interval[1], "C"])
+            c = interval[0][1]
+        elif interval[0][0] >= j:
+            output.append([interval[1], "J"])
+            j = interval[0][1]
         else:
             print("Case #{}: IMPOSSIBLE".format(tt + 1))
             impossible = True
             break
     if not impossible:
-        print("Case #{}: {}".format(tt + 1, output))
+        print("Case #{}: {}".format(tt + 1, ''.join(str(a[1]) for a in sorted(output))))
