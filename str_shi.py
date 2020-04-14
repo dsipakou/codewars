1ea273b8030f539430587fa9aa3dcbb4aad860bd
Dzianis Sipakou <mc.flyer@gmail.com>
2020-04-14 11:03:43 +0300

    End

--- str_shi.py
+++ str_shi.py
@@ -8,5 +8,8 @@ class Solution:
                 num_of_shifts += ss[1]
         if num_of_shifts == 0:
             return s
-        else:
-            return s[-num_of_shifts:] + s[:-num_of_shifts]
+        cc = 1
+        if num_of_shifts < 0:
+            cc = -1
+        num_of_shifts = (abs(num_of_shifts) % len(s)) * cc
+        return s[-num_of_shifts:] + s[:-num_of_shifts]
