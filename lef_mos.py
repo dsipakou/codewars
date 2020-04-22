8e95fc5c06c18ecfa99488fe913642b4b52999fb
Dzianis Sipakou <mc.flyer@gmail.com>
2020-04-22 11:27:31 +0300

    New

---
+++ lef_mos.py
@@ -0,0 +1,27 @@
+# """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class BinaryMatrix(object):
+#    def get(self, x: int, y: int) -> int:
+#    def dimensions(self) -> list[]:
+
+
+
+
+#### O(m*logn)
+class Solution:
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        m, n = binaryMatrix.dimensions()
+        min_n = n + 1
+        for row in range(m):
+            left = 0
+            right = n - 1
+            while left < right:
+                pivot = left + (right - left) // 2
+                if binaryMatrix.get(row, pivot) == 1:
+                    right = pivot
+                else:
+                    left = pivot + 1
+            min_n = min(min_n, right) if binaryMatrix.get(row, right) == 1 else min_n
+        return min_n if min_n < n else -1
