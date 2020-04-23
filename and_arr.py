e9ae35e9163c0abe7578267462381caa0824c531
Dzianis Sipakou <mc.flyer@gmail.com>
2020-04-23 13:18:55 +0300

    Update

--- and_arr.py
+++ and_arr.py
@@ -1,4 +1,8 @@
 class Solution:
     def rangeBitwiseAnd(self, m: int, n: int) -> int:
-        output = reduce(lambda x, y: x & y, range(m, n + 1))
-        return output
+        if len(bin(m)) != len(bin(n)):
+            return 0
+        return reduce(lambda x, y: x & y, range(m, n + 1))
+
+s = Solution()
+print(s.rangeBitwiseAnd(5, 8))
