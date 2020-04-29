1b45b442b9d1df0b9f8f1f228fe6b027cc180903
Dzianis Sipakou <mc.flyer@gmail.com>
2020-04-29 18:09:50 +0300

    New

---
+++ is_sim.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        return self.check(root.left, root.right)
+    
+    def check(self, node1, node2):
+        if node1 is None and node2 is None:
+            return True
+        if node1 is None or node2 is None:
+            return False
+        if node1.val != node2.val:
+            return False
+        return self.check(node1.left, node2.right) and self.check(node1.right, node2.left)
+        
