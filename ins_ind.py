4e4c26892b453002597f605cf17da526147cf424
Dzianis Sipakou <mc.flyer@gmail.com>
2020-06-10 11:26:24 +0300

    update

--- ins_ind.py
+++ ins_ind.py
@@ -1,5 +1,15 @@
+class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for i, v in enumerate(nums):
-            if target <= v:
-                return i
-        return len(nums) - 1
+        nums = [float('-inf')] + nums + [float('inf')]
+        print(nums)
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            if target == nums[mid]:
+                return mid - 1
+            elif nums[mid] > target:
+                right = mid
+            else:
+                left = mid + 1
+        return right - 1
