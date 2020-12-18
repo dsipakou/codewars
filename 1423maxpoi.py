3cafd2d9bc2850c9d5338660009eb2743c07f94e
Dzianis Sipakou <mc.flyer@gmail.com>
2020-12-18 16:39:59 +0300

    Update 1423maxpoi.py

--- 1423maxpoi.py
+++ 1423maxpoi.py
@@ -1,9 +1,14 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        global_max = 0
         if k >= len(cardPoints):
             return sum(cardPoints)
-        for i in range(k + 1):
-            current_sum = sum(cardPoints[: k - i]) + sum(cardPoints[len(cardPoints) - i:])
-            global_max = max(global_max, current_sum)
+        global_sum = sum(cardPoints[: k])
+        global_max = global_sum
+        left_sum = global_sum
+        right_sum = 0
+        for i in range(k):
+            left_sum -= cardPoints[k - 1 - i]
+            right_sum += cardPoints[0 - 1 - i]
+            global_max = max(global_max, (left_sum + right_sum))
         return global_max
+    
