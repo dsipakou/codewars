27035fad585321b7d492824bd086a765f0f1cd1e
Dzianis Sipakou <mc.flyer@gmail.com>
2021-01-09 20:52:02 +0300

    Update 217condup.py

--- 217condup.py
+++ 217condup.py
@@ -1,9 +1,10 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
+        d = dict()
         i = 0
-        while i < len(nums) - 1:
-            if nums[i] == nums[i + 1]:
+        while i < len(nums):
+            if nums[i] in d:
                 return True
+            d[nums[i]] = True
             i += 1
         return False
