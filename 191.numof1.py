2b58d37418ca4e7c6c52f5be19231a7f728c35a0
Dzianis Sipakou <mc.flyer@gmail.com>
2021-02-01 21:50:01 +0300

    Update 191.numof1.py

--- 191.numof1.py
+++ 191.numof1.py
@@ -1,3 +1,14 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         return (bin(n)[2:].count('1'))
+
+## With bitwise ops
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        output = 0
+        for _ in range(32):
+            if n & 1:
+                output += 1
+            n = n >> 1
+        return output
