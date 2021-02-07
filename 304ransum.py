8e0aafa9fe91fcb7158a912d81ebb9ac7ec8e952
Dzianis Sipakou <mc.flyer@gmail.com>
2021-02-07 22:04:46 +0300

    Update 304ransum.py

--- 304ransum.py
+++ 304ransum.py
@@ -1,18 +1,19 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.arr = matrix
-        
+        self.arr = []
+        for i in range(len(matrix)):
+            cur = 0
+            temp = [0]
+            for j in range(len(matrix[0])):
+                cur += matrix[i][j]
+                temp.append(cur)
+            self.arr.append(temp)
+            temp = []        
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         output = 0
         for i in range(row1, row2 + 1):
-            for j in range(col1, col2 + 1):
-                output += self.arr[i][j]
+            divv = self.arr[i][col2+1] - self.arr[i][col1]
+            output += divv
         return output
-        
-
-
-# Your NumMatrix object will be instantiated and called as such:
-# obj = NumMatrix(matrix)
-# param_1 = obj.sumRegion(row1,col1,row2,col2)
