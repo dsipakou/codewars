e148757696d00c4b780481d05859c144fd415428
Dzianis Sipakou <mc.flyer@gmail.com>
2021-02-08 21:47:49 +0300

    Update 1405lonhap.py

--- 1405lonhap.py
+++ 1405lonhap.py
@@ -2,16 +2,25 @@ class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
         output = ''
         count = [[a, 'a'], [b, 'b'], [c, 'c']]
-        i = j = k = 0
+        i = 1
         
-        while count[0][0] >= 0 and count[1][0] >= 0 and count[2][0] >= 0:
+        while count[0][0] > 0 or count[1][0] > 0 or count[2][0] > 0:
+
             count.sort(reverse=True)
-            print(count)
-            output += count[0][1]
-            count[0][0] -= 1
-            if output[-1] == count[0][1]:
-                i += 1
+            if len(output) > 0 and count[0][1] == output[-1]:
+                if i == 2:
+                    if count[1][0] > 0:
+                        output += count[1][1]
+                        count[1][0] -= 1
+                        i = 1
+                    else:
+                        break
+                else:
+                    output += count[0][1]
+                    i += 1
+                    count[0][0] -= 1
             else:
                 i = 1
-            print(i)
-        return outputtput
+                output += count[0][1]
+                count[0][0] -= 1
+        return output
