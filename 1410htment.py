558729fc9b8c6fb47e30397d006b0c6103e446af
Dzianis Sipakou <mc.flyer@gmail.com>
2021-02-12 21:56:34 +0300

    Update 1410htment.py

--- 1410htment.py
+++ 1410htment.py
@@ -8,21 +8,8 @@ class Solution:
             "&lt;": "<",
             "&frasl;": "/"
         }
-        output = ''
-        i = 0
-        while i < len(text):
-            if text[i] == "&":
-                end = text[i:].index(';')
-                sub = text[i:end+i+1]
-                print(sub, d.get(sub))
-                if sub in d:
-                    output += d[sub]
-                    i = end+i+1
-                else:
-                    output += text[i]
-                    i += 1
-            else:
-                output += text[i]
-                i += 1
-        return output
+        tmp = "!@#$%"
+        for k, v in d.items():
+            text = text.replace(k, v+tmp)
+        return text.replace(tmp, '')
                 
