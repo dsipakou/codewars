c3231d0111a84c71afc692def8d51be70fa1c28c
Dzianis Sipakou <mc.flyer@gmail.com>
2021-02-16 22:01:39 +0300

    Update 1442coutri.py

--- 1442coutri.py
+++ 1442coutri.py
@@ -1,13 +1,10 @@
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
         output = 0
-        for outer in range(len(arr)):
-            a = 0
-            for i in range(outer, len(arr) - 1):
-                a ^= arr[i]
-                b = 0
-                for j in range(i + 1, len(arr)):
-                    b ^= arr[j]
-                    if a == b:
-                        output += 1
+        for i in range(len(arr)):
+            a = arr[i]
+            for k in range(i + 1, len(arr)):
+                a ^= arr[k]
+                if a == 0:
+                    output += k - i
         return output
