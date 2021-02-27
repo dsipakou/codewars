eec075e80a0d3a5e10bdbab54a5563e65600c434
Dzianis Sipakou <mc.flyer@gmail.com>
2021-02-27 19:12:58 +0300

    Create 389finthe.py

---
+++ 389finthe.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s=sorted(s)
+        t=sorted(t)
+        for i in range(len(t) - 1):
+            if s[i] != t[i]:
+                return t[i]
+        return t[-1]
