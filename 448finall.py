355e48a3e3aac9da23c181b28b24bd6b44923c94
Dzianis Sipakou <mc.flyer@gmail.com>
2021-04-10 22:20:17 +0300

    Create 448finall.py

---
+++ 448finall.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            if nums[abs(nums[i]) - 1] > 0:
+                nums[abs(nums[i]) - 1] = -nums[abs(nums[i]) - 1]
+        res = []
+        for i, v in enumerate(nums):
+            if v > 0:
+                res.append(i + 1)
+        return res
+            
