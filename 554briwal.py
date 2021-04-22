9196f988e0e5cd63e3817187f0c861d3bf501b13
Dzianis Sipakou <mc.flyer@gmail.com>
2021-04-22 20:42:56 +0300

    Update 554briwal.py

--- 554briwal.py
+++ 554briwal.py
@@ -5,14 +5,20 @@ class Solution:
         d = {}
         for i in range(len(wall)):
             d[i] = {"index": 0, "value": wall[i][0]}
-        for i in range(1, gen_len + 1):
+        i = 0
+        while i < gen_len:
+            print(i)
+            jump = gen_len
             gaps = 0
             for j in range(len(wall)):
                 if d[j]["value"] == 0:
                     gaps += 1
                     d[j]["index"] += 1
                     d[j]["value"] = wall[j][d[j]["index"]]
-                else:
-                    d[j]["value"] -= 1
+                
+                jump = min(jump, d[j]["value"])
+            for j in range(len(wall)):
+                d[j]["value"] -= jump
+            i += jump
             output = min(output, len(wall) - gaps)
         return output
