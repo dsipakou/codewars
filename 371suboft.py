6527225634419e990cd8ec384885a08f8b6b5b1a
Dzianis Sipakou <mc.flyer@gmail.com>
2021-05-31 17:21:38 +0300

    Update 371suboft.py

--- 371suboft.py
+++ 371suboft.py
@@ -1,32 +1,8 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        bin_a = bin(a)[2:]
-        bin_b = bin(b)[2:]
-        bin_output = []
-        max_len = max(len(bin_a), len(bin_b))
-        bin_a = bin_a.rjust(max_len, '0')
-        bin_b = bin_b.rjust(max_len, '0')
-        curry = False
-        for i in range(len(bin_a) - 1, -1, -1):
-            if bin_a[i] == '1' or bin_b[i] == '1':
-                if bin_a[i] == bin_b[i]:
-                    if curry:
-                        bin_output.append('1')
-                    else:
-                        bin_output.append('0')
-                    curry = True
-                else:
-                    if curry:
-                        bin_output.append('0')
-                    else:
-                        bin_output.append('1')
-            else:
-                print('else')
-                if curry:
-                    bin_output.append('1')
-                else:
-                    bin_output.append('0')
-                curry = False
-        if curry:
-            bin_output.append('1')
-        return int(''.join(i for i in reversed(bin_output)), base=2)
+        xor_temp = a ^ b
+        curry = a & b
+        if curry == 0:
+            return xor_temp
+        else:
+            return self.getSum(xor_temp, curry << 1)
