1980bbdc04a08f76ea8371c196d8019ee82b19b4
Dzianis Sipakou <mc.flyer@gmail.com>
2021-06-13 18:51:41 +0300

    Create 513finbot.py

---
+++ 513finbot.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: TreeNode) -> int:
+        queue = [root]
+        while queue:
+            current = queue.pop(0)
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+        return current.val
