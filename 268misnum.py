4bec52f613c349924c5da94beeb65f842f579fe8
Dzianis Sipakou <mc.flyer@gmail.com>
2021-07-01 19:43:37 +0300

    Update 268misnum.py

--- 268misnum.py
+++ 268misnum.py
@@ -1,15 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        loc_sum = 0
-        glo_sum = 0
-        flag = False
-        for i, v in enumerate(nums):
-            glo_sum += i
-            if v == len(nums):
-                flag = True    
-                glo_sum += v
-            loc_sum += v
-        ret = glo_sum - loc_sum
-        if ret == 0 and not flag:
-            return len(nums)
-        return ret
+        loc_sum = sum(nums)
+        glo_sum = sum([i for i in range(len(nums) + 1)])
+        return glo_sum - loc_sum
