e7f2e7d475e11ed6f3e4e14543e787e10479ed5b
Dzianis Sipakou <mc.flyer@gmail.com>
2022-06-06 19:34:11 +0300

    Create 62unipar.py

---
+++ 62unipar.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+        for i in range(m - 1):
+            new_row = [1] * n
+            for j in range(1, n):
+                new_row[j] = row[j] + new_row[j - 1]
+            row = new_row
+        return row[-1]
